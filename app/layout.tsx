5dc665cedefa0f9228504d4414694020861b502a
Guillaume EGU <158036798+Guigz2@users.noreply.github.com>
2025-08-30 21:35:56 +0000

    SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,65 @@ import BottomNav from '@/components/HBF/BottomNav'
 import HeaderSwitch from '@/components/HBF/HeaderSwitch'
 
 export const metadata: Metadata = {
-  title: 'Guillaume EGU',
-  description: 'Portfolio de Guillaume EGU a but de présentation professionnelle',
+  metadataBase: new URL('https://eguguillaume.com'),
+  title: {
+    default : 'Guillaume EGU',
+    template : '%s | Guillaume EGU',
+  },
+  description: 'Portfolio de Guillaume EGU – Jeune diplomé en recherche d\'emploi dans le monde de la data science',
+  applicationName: 'Guillaume EGU',
+  authors: [{ name: 'Guillaume EGU', url: 'https://eguguillaume.com' }],
+  generator: 'Next.js',
+  keywords: [
+    'Guillaume EGU', 
+    'Portfolio', 
+    'Data Scientist', 
+    'Machine Learning', 
+    'Python', 
+    'SQL',  
+    'Git', 
+    'GitHub',
+  ],
+  creator: 'Guillaume EGU',
+  publisher: 'Guillaume EGU',
+  alternates: {
+    canonical: "https://www.eguguillaume.com/",
+    languages: {
+      'fr-FR': '/',
+    },
+  },
+  openGraph: {
+    type: 'website',
+    url: 'https://eguguillaume.com',
+    title: 'Guillaume EGU',
+    siteName: "Guillaume EGU",
+    description: 'Portfolio de Guillaume EGU – Jeune diplomé en recherche d\'emploi dans le monde de la data science',
+    images: [
+      {
+        url: '/og-image.png',
+        width: 1200,
+        height: 630,
+        alt: 'Guillaume EGU',
+      },
+    ],
+    locale: 'fr_FR',
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-image-preview':  'large',
+      'max-video-preview': -1,
+      'max-snippet': -1,
+    },
+  },
+  verification: {
+    google: 'google-site-verification: google0de347b8cf9d05b1.html',
+  },
+  category: 'portfolio',
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
