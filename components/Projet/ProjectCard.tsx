de4040eb86e3ec5ab26149f9fb020de086ddb8d9
Guillaume EGU <158036798+Guigz2@users.noreply.github.com>
2025-08-31 22:04:31 +0000

    PRELOAD Projets

--- components/Projet/ProjectCard.tsx
+++ components/Projet/ProjectCard.tsx
@@ -4,9 +4,9 @@ import type { Project } from '@/content/projects'
 
 export default function ProjectCard({ project }: { project: Project }) {
   return (
-    <Link href={`/projets/${project.slug}`} className="block group border border-black">
+    <Link href={`/projets/${project.slug}`} className="block group border border-black bg-white hover:shadow-lg transition-shadow">
       <div className="relative aspect-[4/3] overflow-hidden">
-        <Image src={project.image} alt={project.title} fill className="object-cover transition-transform duration-300 group-hover:scale-105" />
+        <Image src={project.image} alt={project.title} fill className="object-contain transition-transform duration-300 group-hover:scale-105" />
       </div>
       <div className="p-4 bg-black text-white">
         <h2 className="text-xl font-bold">{project.title}</h2>
